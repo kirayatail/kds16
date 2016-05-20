6aec5b1608212a17c89c61bcc9ddc39e378a017d
Max Witt <kirayatail@gmail.com>
2016-05-20 10:32:42 +0200

    HTML form done for registering users, backend connection and feedback pending.

---
+++ frontend/registeruser/registration.js
@@ -0,0 +1,63 @@
+var app = angular.module('knowitdevsummit');
+
+app.directive('registeruser', ['$http', function($http) {
+
+  return {
+    templateUrl: 'registeruser/registeruser.html',
+    link: function(scope) {
+      scope.form = [
+        {
+          label: "Email",
+          id: "email"
+        },
+        {
+          label: "First name",
+          id: "firstname"
+        },
+        {
+          label: "Last name",
+          id: "lastname"
+        },
+        {
+          label: "Phone number",
+          id: "phone"
+        },
+        {
+          label: "Knowit company",
+          id: "company"
+        },
+        {
+          label: "Invoice identifier",
+          id: "invoice"
+        },
+        {
+          label:"Food allergies or requirements",
+          id: "foodreqs"
+        }
+      ];
+
+      scope.roles =
+        {
+          "Developer": false,
+          "Tester/test lead": false,
+          "Project manager": false,
+          "Architect": false,
+          "UX specialist": false,
+          "Security professional": false,
+          "Product developer": false,
+          "Manager": false,
+          "Scrum master": false,
+          "Agile coach": false,
+          "Designer": false,
+          "Other": false
+        };
+      scope.gender = [
+        'male',
+        'female',
+        'other'
+      ];
+
+      scope.user = {};
+    }
+  }
+}]);
